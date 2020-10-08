d7dd2ce5e4bbbae2c6c87f18ad04e3048c7f5d86
Ranu311 <71586938+Ranu311@users.noreply.github.com>
2020-10-08 14:42:56 +0530

    Update c-welcome.c

--- c-welcome.c
+++ c-welcome.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 #include<cstrings.h>
 
-main()
+int main()
 {
+  int roll;
   string name;
   printf("Enter your name: \n");
   scanf("%s\n",@name);
+  printf("Enter Your Roll No.:  \n"
+  scanf("%d\n",&roll);
+    
   printf("Welcome \n",name);
+  printf("Your Roll No. is %d \n",roll);
+  return 0;
 }
